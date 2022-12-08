a27d047c991d6f58628c2205c326427721351c5a
yue7872 <2258462872@qq.com>
2022-12-08 18:08:01 +0800

    feat: 添加简单示例

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # [name]
 
 <a href="https://marketplace.visualstudio.com/items?itemName=blairyue.[name]" target="__blank"><img src="https://img.shields.io/visual-studio-marketplace/v/[name].svg?color=blue&amp;label=VS%20Code%20Marketplace&logo=visual-studio-code" alt="Visual Studio Marketplace Version" /></a>
-
-## License
-
-[MIT](./LICENSE) License © 2022 [yue7872](https://github.com/yue7872)
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import { window } from 'vscode'
+import type { ExtensionContext } from 'vscode'
+import { commands, window, workspace } from 'vscode'
 
-export function activate() {
-  window.showInformationMessage('Hello')
+export function activate(context: ExtensionContext) {
+  const disposable = commands.registerCommand('[name]', () => {
+    const openWindow = window.createTerminal('[name]')
+    const projectRoot = workspace.workspaceFolders![0].uri.path
+
+    // ssh地址转换为https地址
+    openWindow.sendText(`ls`)
+    openWindow.show()
+    window.showInformationMessage(`hello world! ${projectRoot}`)
+  })
+
+  context.subscriptions.push(disposable)
 }
 
 export function deactivate() {
